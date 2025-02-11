205f8425480a9bbb035fb555b3262860b5ff428c
PedroDelgo <pedrodelg.go@gmail.com>
2025-02-11 16:15:16 -0300

    feat: creates header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-function App() {
+import React from 'react'
+import Header from './components/Header'
+
+const App = () => {
   return (
-    <></>
+    <Header/>
   )
 }
+
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import logoSpotify from '../assets/ico-spotify.png'
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+
+        <div className="headerLeft">
+            <img id='logospotify' src= {logoSpotify} alt="Spotify Logo"/>
+        </div>
+
+        <div className="headerRight">
+            <a href="">
+                <h1>Spotify</h1>
+            </a>
+        </div>
+
+    </div>
+  )
+}
+export default Header
